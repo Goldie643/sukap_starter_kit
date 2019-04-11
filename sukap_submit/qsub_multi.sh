7640559a9bcd3455f35b68bf59bcc2e66d81f52f
Alexander Goldsack <goldsack@sukap001v301.km.icrr.u-tokyo.ac.jp>
2019-04-11 14:03:53 +0900

    Fixed non-nuance submit scripts, works fine if both scripts are in current directory

--- sukap_submit/qsub_multi.sh
+++ sukap_submit/qsub_multi.sh
@@ -1,11 +1,18 @@
-export card_dir=/disk01/usr4/goldsack/test_cards
-export card_file=$card_dir/2mev_positron_NONOISE.card
+export card_file=$1
 export skds=/disk01/usr4/goldsack/skdetsim/skdetsim.sh
+qsub_file=/disk01/usr4/goldsack/sds_io/qsub_sds_submit.sh
 
-for i in {1..10}
+n_per_file=`grep VECT-NEVT $card_file`
+echo "n_files (of VECT-NEVT events in .card file):"
+echo $n_per_file
+read n_files
+echo "Out file name prefix:"
+read prefix
+
+for i in $(seq 1 ${n_files})
 do
     sed -i "s/^VECT-RAND.*/VECT-RAND  $RANDOM  $RANDOM  0  0  0/g" $card_file
-    export out_file="2mev_positron_NONOISE_$i.root"
+    export out_file="${prefix}${i}.root"
     echo $out_file
-    qsub -x  $card_dir/qsub_sds_submit.sh
+    qsub -x $qsub_file 
 done
--- sukap_submit/qsub_sds_submit.sh
+++ sukap_submit/qsub_sds_submit.sh
@@ -1,13 +1,19 @@
 source /usr/local/sklib_gcc4.8.5/skofl_16c/env.sh
 
-card_dir=/disk01/usr4/goldsack/sds_io
-card_file=$card_dir/reactor_positron.card
-nuance_file=$card_dir/reactor_s20000.nuance
+card_dir=/disk01/usr4/goldsack/sds_io/card_files
 skds=/disk01/usr4/goldsack/skdetsim/skdetsim.sh
 
-if [ -z $out_file]
+if [ -z "$out_file" ]
 then 
     out_file=$card_dir/"reactor_positron.root"
 fi
+
+if [ -z "$card_file" ]
+then 
+    card_file=$card_dir/"reactor_positron.card"
+fi
+
+echo "Card file: $card_file"
+echo "Out file: $out_file"
 
 $skds $card_file $out_file
